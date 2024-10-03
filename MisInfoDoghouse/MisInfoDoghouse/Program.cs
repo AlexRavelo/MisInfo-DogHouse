a73d9ca63d46c590ccba8c082d5e0ad2adbd6d12
Alex Ravelo <alexandooop@gmail.com>
2024-10-03 17:49:23 -0400

    Initialization

---
+++ MisInfoDoghouse/MisInfoDoghouse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MisInfoDoghouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the MisInfo Doghouse. It will do stuff once development starts rolling!");
+        }
+    }
+}
